9198bd86ccf3b317efb2315062e2d5688b745393
Shamim Ahmed Nahid <shamimofficial771@gmail.com>
2026-05-08 02:06:08 +0600

    Add files via upload

---
+++ console.log.py
@@ -0,0 +1,12 @@
+""" JavaScript's console.log("Hello World!"); in Python """
+
+class console:
+
+    @staticmethod
+    def log(text):
+        print(text)
+
+console = console()
+
+if __name__ == "__main__":
+    console.log("Hello World!"); # Output: Hello World!
