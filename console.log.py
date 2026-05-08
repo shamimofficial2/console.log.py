d733285181177ebe0fc2f8f4225a71f366cce598
Shamim Ahmed Nahid <shamimofficial771@gmail.com>
2026-05-08 11:26:46 +0600

    Refactor console.log to use sys.stdout.write
    Updated console.log method to use sys.stdout.write for output.

--- console.log.py
+++ console.log.py
@@ -1,10 +1,12 @@
 """ JavaScript's console.log("Hello World!"); in Python """
 
+import sys
+
 class console:
 
     @staticmethod
-    def log(text: str) -> str:
-        print(text)
+    def log(text: str) -> None:
+        sys.stdout.write(f"{text}\n")
 
 console = console()
 
