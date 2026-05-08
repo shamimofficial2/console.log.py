6265835ea06639c52a151ceb44c17524b06432e5
Shamim Ahmed Nahid <shamimofficial771@gmail.com>
2026-05-08 11:29:46 +0600

    Refactor console class to use PascalCase

--- console.log.py
+++ console.log.py
@@ -2,13 +2,13 @@
 
 import sys
 
-class console:
+class Console:
 
     @staticmethod
     def log(text: str) -> None:
         sys.stdout.write(f"{text}\n")
 
-console = console()
+console = Console()
 
 if __name__ == "__main__":
     console.log("Hello World!"); # Output: Hello World!
